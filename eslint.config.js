6d4109d39f506e259d067cc37451df9e9c1b4aec
Santiago Molina Orozco <santi020k@gmail.com>
2025-02-04 23:56:01 +0100

    feat(eslint.config.js): add @stencil-community support and fix ignore files
    ignore files with the .gitignore file

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
 // @ts-check
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { fixupConfigRules, includeIgnoreFile } from '@eslint/compat'
+import { FlatCompat } from '@eslint/eslintrc'
 import { ConfigOption, eslintConfig, OptionalOption } from '@santi020k/eslint-config-santi020k'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+const flatCompat = new FlatCompat()
+
 export default [
-  {
-    name: 'ignore-local',
-    ignores: [
-      '**/packages/components/.stencil/**',
-      '**/packages/components/dist/**',
-      '**/packages/components/loader/**',
-      '**/packages/components/www/**',
-      '**/packages/hydrate/**',
-      '**/packages/react/**',
-      '**/packages/website/.docusaurus/**'
-    ]
-  },
+  includeIgnoreFile(gitignorePath),
+  ...fixupConfigRules(flatCompat.plugins('@stencil-community')),
   ...eslintConfig({
     config: [ConfigOption.Ts],
     optionals: [OptionalOption.Cspell, OptionalOption.Vitest]
