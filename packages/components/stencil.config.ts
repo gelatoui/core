82d2d6df22932acf0c419ccbddee2748588e7f43
Santiago Molina Orozco <santi020k@gmail.com>
2025-02-04 16:24:08 +0100

    Initial stencil

---
+++ packages/components/stencil.config.ts
@@ -0,0 +1,36 @@
+import { Config } from '@stencil/core';
+import { OutputTarget } from '@stencil/core/internal';
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+export const config: Config = {
+  namespace: 'components',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    reactOutputTarget({
+      outDir: '../react/src/components/',
+      hydrateModule: '@gelato-ui/hydrate'
+    }) as unknown as OutputTarget,
+    {
+      type: 'dist-hydrate-script',
+      dir: './hydrate',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "shell",
+  },
+};
